ada574f412a2938ac600dcf03f17594889c0f1ac
Chirag P Patil <chiragpatil07@gmail.com>
2025-06-19 21:48:19 +0530

    Update run config for local and Render compatibility

--- app.py
+++ app.py
@@ -728,4 +728,5 @@ def update_predicted_risks_display(selected_city):
 
 # --- Run the App ---
 if __name__ == '__main__':
-    app.run_server(debug=True)
+    port = int(os.environ.get("PORT", 8050))
+    app.run_server(host='0.0.0.0', port=port, debug=True)
